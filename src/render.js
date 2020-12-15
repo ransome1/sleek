445507cc7aac568c582feda965ed2489729653d6
ransome1 <spam-github@datenkrake.eu>
2020-12-15 12:27:05 +0100

    flatpak trial and error

--- src/render.js
+++ src/render.js
@@ -1,6 +1,42 @@
 // ########################################################################################################################
 // DECLARATIONS
 // ########################################################################################################################
+const fs = require("fs");
+const path = require('path');
+const i18next = require('i18next');
+const i18nextBackend = require('i18next-fs-backend');
+const i18nextBrowserLanguageDetector = require('i18next-browser-languagedetector');
+const detectionOptions = {
+  order: ['querystring', 'navigator'],
+  lookupQuerystring: 'lng'
+}
+const i18nextOptions = {
+  initImmediate: false,
+  fallbackLng: "en",
+  detection: detectionOptions,
+  namespace: "translation",
+  defaultNS: "translation",
+  supportedLngs: ['de', 'en', 'it'],
+  debug: false,
+  preload: fs.readdirSync(path.join(__dirname, '../locales')).filter((fileName) => {
+    const joinedPath = path.join(path.join(__dirname, '../locales'), fileName)
+    const isDirectory = fs.lstatSync(joinedPath).isDirectory()
+    return isDirectory
+  }),
+  backend: {
+    loadPath: path.join(__dirname, '../locales/{{lng}}/{{ns}}.json'),
+    addPath: path.join(__dirname, '../locales/{{lng}}/{{ns}}.missing.json')
+  },
+  saveMissing: true
+};
+i18next
+  .use(i18nextBackend)
+  .use(i18nextBrowserLanguageDetector)
+if (!i18next.isInitialized) {
+  i18next
+    .init(i18nextOptions);
+}
+
 const documentIds = document.querySelectorAll("[id]");
 documentIds.forEach(function(id,index) {
   window[id] = document.getElementById(id.getAttribute("id"));
