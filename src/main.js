46cee0cc4ac105e8b0cd75db7cf4d724d5573dc4
ransome1 <spam-github@datenkrake.eu>
2020-12-15 11:54:21 +0100

    flatpak fixing

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const path = require("path");
 }*/
 const isMac = process.platform === "darwin";
 const Store = require("./configs/store.config.js");
-const i18next = require("./configs/i18next.config");
+//const i18next = require("./configs/i18next.config");
 
 let mainWindow; //do this so that the window object doesn"t get GC"d
 
@@ -61,36 +61,50 @@ const createWindow = () => {
   // Template for menu
   const menuTemplate = [
     {
-      label: i18next.t("file"),
+      //label: i18next.t("file"),
+      label: "File",
       submenu: [
         {
-          label: i18next.t("openFile"),
+          //label: i18next.t("openFile"),
+          label: "Open todo.txt file",
           accelerator: "CmdOrCtrl+o",
           click: function (item, focusedWindow) {
             mainWindow.webContents.executeJavaScript("openFile()");
           }
         },
         {
-          label: i18next.t("onboardingContainerBtnCreate"),
+          //label: i18next.t("onboardingContainerBtnCreate"),
+          label: "Create new todo.txt",
           click: function (item, focusedWindow) {
             mainWindow.webContents.executeJavaScript("createFile(true, false)");
           }
         },
-        isMac ? { role: "quit", label: i18next.t("close") } : { role: "close", label: i18next.t("close") }
+        isMac ? {
+          role: "quit",
+          //label: i18next.t("close")
+          label: "Close"
+        } : {
+          role: "close",
+          //label: i18next.t("close")
+          label: "Close"
+        }
       ]
     },
     {
-      label: i18next.t("todos"),
+      //label: i18next.t("todos"),
+      label: "Todos",
       submenu: [
         {
-          label: i18next.t("addTodo"),
+          //label: i18next.t("addTodo"),
+          label: "Add a new todo",
           accelerator: "CmdOrCtrl+n",
           click: function (item, focusedWindow) {
             mainWindow.webContents.executeJavaScript("showForm(true)");
           }
         },
         {
-          label: i18next.t("find"),
+          //label: i18next.t("find"),
+          label: "Find",
           accelerator: "CmdOrCtrl+f",
           click: function (item, focusedWindow) {
             mainWindow.webContents.executeJavaScript("todoTableSearch.focus()");
@@ -99,40 +113,46 @@ const createWindow = () => {
       ]
     },
     {
-      label: i18next.t("view"),
+      //label: i18next.t("view"),
+      label: "View",
       submenu: [
         {
-          label: i18next.t("toggleDarkMode"),
+          //label: i18next.t("toggleDarkMode"),
+          label: "Toggle dark mode",
           accelerator: "CmdOrCtrl+d",
           click: function (item, focusedWindow) {
             mainWindow.webContents.executeJavaScript("switchTheme(\"dark\")");
           }
         },
-        /*{
-          label: i18next.t("toggleFilter"),
-          accelerator: "CmdOrCtrl+f",
-          click: function (item, focusedWindow) {
-            mainWindow.webContents.executeJavaScript("showFilters(\"toggle\")");
-          }
-        },*/
         {
-          label: i18next.t("toggleCompletedTodos"),
+          //label: i18next.t("toggleCompletedTodos"),
+          label: "Toggle completed todos",
           accelerator: "CmdOrCtrl+h",
           click: function (item, focusedWindow) {
             mainWindow.webContents.executeJavaScript("showCompletedTodos()");
           }
         },
-        {role: "reload", label: i18next.t("reload")}
+        {
+          role: "reload",
+          label: "Reload"
+          //label: i18next.t("reload")
+        }
       ]
     },
     {
-      label: i18next.t("about"),
+      //label: i18next.t("about"),
+      label: "About",
       submenu: [
         {
-          label: i18next.t("sleekOnGithub"),
+          //label: i18next.t("sleekOnGithub"),
+          label: "sleek on Github",
           click: () => {require("electron").shell.openExternal("https://github.com/ransome1/sleek")}
         },
-        {role: "toggleDevTools", label: i18next.t("devTools")}
+        {
+          role: "toggleDevTools",
+          //label: i18next.t("devTools")
+          label: "Toggle developer tools"
+        }
       ]
     }
   ];
