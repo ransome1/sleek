46dd0b4ea5888ce9b7c4ec8867f9fb371b6c59f7
PapuleX <32964290+PapuleX@users.noreply.github.com>
2021-10-04 20:52:11 +0200

    Incomplete Czech

--- src/configs/i18next.config.js
+++ src/configs/i18next.config.js
@@ -7,7 +7,7 @@ const i18nextOptions = {
   fallbackLng: "en",
   namespace: "translation",
   defaultNS: "translation",
-  supportedLngs: ["de", "en", "it", "es", "fr", "zh", "pt", "jp", "tr", "hu"],
+  supportedLngs: ["de", "en", "it", "es", "fr", "zh", "pt", "jp", "tr", "hu", "cs"],
   debug: false,
   preload: fs.readdirSync(path.join(__dirname, "../locales")).filter((fileName) => {
     const joinedPath = path.join(path.join(__dirname, "../locales"), fileName)
