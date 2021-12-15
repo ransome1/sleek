89591908d3b505e45098fe6b9388cd8c41362b09
ransome1 <spam-github@datenkrake.eu>
2021-12-15 08:28:17 +0100

    Added env for multiple instances

--- src/main.js
+++ src/main.js
@@ -834,27 +834,42 @@ const createWindow = async function() {
 const gotTheLock = app.requestSingleInstanceLock()
 // prevent multiple instances based on https://stackoverflow.com/questions/35916158/how-to-prevent-multiple-instances-in-electron
 if (!gotTheLock) {
-  app.quit()
-} else {
-  app.on('second-instance', (event, commandLine, workingDirectory) => {
-    // Someone tried to run a second instance, we should focus our window.
-    if (mainWindow) {
-      if (mainWindow.isMinimized()) mainWindow.restore()
-      mainWindow.focus()
+    if(process.env.SLEEK_MULTIPLE_INSTANCES==="true") {
+        app.on("ready", () => {
+            if (appData.os === "windows") app.setAppUserModelId("RobinAhle.sleektodomanager")
+            createWindow();
+            if (appData.channel === "AppImage") autoUpdater.checkForUpdatesAndNotify();
+        })
+        .on("window-all-closed", () => {
+            if (process.platform !== "darwin") app.quit()
+            mainWindow = null;
+        })
+        .on("activate", () => {
+            if (BrowserWindow.getAllWindows().length === 0) createWindow()
+            app.show();
+        });
+    } else {
+        app.quit();
     }
-  })
-  app
-    .on("ready", () => {
-      if (appData.os === "windows") app.setAppUserModelId("RobinAhle.sleektodomanager")
-      createWindow();
-      if (appData.channel === "AppImage") autoUpdater.checkForUpdatesAndNotify();
+} else {
+    app.on("second-instance", (event, commandLine, workingDirectory) => {
+        // Someone tried to run a second instance, we should focus our window.
+        if (mainWindow) {
+        if (mainWindow.isMinimized()) mainWindow.restore()
+        mainWindow.focus()
+        }
+    })
+    app.on("ready", () => {
+        if (appData.os === "windows") app.setAppUserModelId("RobinAhle.sleektodomanager")
+        createWindow();
+        if (appData.channel === "AppImage") autoUpdater.checkForUpdatesAndNotify();
     })
     .on("window-all-closed", () => {
-      if (process.platform !== "darwin") app.quit()
-      mainWindow = null;
+        if (process.platform !== "darwin") app.quit()
+        mainWindow = null;
     })
     .on("activate", () => {
-      if (BrowserWindow.getAllWindows().length === 0) createWindow()
-      app.show();
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+        app.show();
     });
 }
