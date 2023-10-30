611e2ee4a2e59ab95f0f8293e52946f7bedbf59d
ransome1 <ransome@duck.com>
2023-10-30 17:49:18 +0100

    Updated build Git workflow, bumped version number, updated Flathub prep process

--- .erb/scripts/update-version-numbers.js
+++ .erb/scripts/update-version-numbers.js
@@ -1,18 +1,15 @@
 const fs = require('fs');
 const packageJson = require('../../release/app/package.json');
-const { format } = require('date-fns'); // You may need to install this library using `yarn add date-fns` if you haven't already.
+const dayjs = require('dayjs');
 
-// Update the .flatpak/com.github.ransome1.sleek.desktop file
 let desktopContent = fs.readFileSync('../../flatpak/com.github.ransome1.sleek.desktop', 'utf8');
 desktopContent = desktopContent.replace(/Version=.*\n/, `Version=${packageJson.version}\n`);
 fs.writeFileSync('../../flatpak/com.github.ransome1.sleek.desktop', desktopContent);
 
 console.log('Updated com.github.ransome1.sleek.desktop with version', packageJson.version);
 
-// Update the com.github.ransome1.sleek.appdata.xml file
 let appdataContent = fs.readFileSync('../../flatpak/com.github.ransome1.sleek.appdata.xml', 'utf8');
-const today = new Date();
-const formattedDate = format(today, 'yyyy-MM-dd');
+const formattedDate = dayjs(new Date()).format('YYYY-MM-DD');
 appdataContent = appdataContent.replace(/<release version=".*?" date=".*?"\/>/, `<release version="${packageJson.version}" date="${formattedDate}"/>`);
 fs.writeFileSync('../../flatpak/com.github.ransome1.sleek.appdata.xml', appdataContent);
 
