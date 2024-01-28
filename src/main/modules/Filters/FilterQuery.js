9f438da3b5cf8958dc69a5e5b606d4b3dc3cc454
zerodat <81216634+zerodat@users.noreply.github.com>
2024-01-28 13:46:01 -0800

    solves filter bug due to todoObject.toString, issue #647

--- src/main/modules/Filters/FilterQuery.js
+++ src/main/modules/Filters/FilterQuery.js
@@ -71,11 +71,11 @@ function runQuery(todoObject, compiledQuery) {
         break;
       case "string":
         next = q.shift();  // the string value to match
-        stack.push(todoObject.toString().toLowerCase().indexOf(next.toLowerCase()) !== -1);
+        stack.push(todoObject.string.toLowerCase().indexOf(next.toLowerCase()) !== -1);
         break;
       case "regex":
         next = q.shift();  // the regex to match
-        stack.push(next.test(todoObject.toString()));
+        stack.push(next.test(todoObject.string));
         break;
       case "==":
         operand2 = stack.pop();
